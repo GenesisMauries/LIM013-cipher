70f6707afaa68d557cc3bdd8655ecc38ba9c69d4
Génesis Mauries♫ <exodo_54@hotmail.com>
2020-12-23 11:40:52 -0600

    Primer version

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,53 @@
 const cipher = {
-  // ...
+  encode: (offset, string) => {
+    if (offset === null || offset === 0) {
+      throw new TypeError();
+    }
+    let string2 = '';
+    // Recorrer el string
+    for (let i = 0; i < string.length; i += 1) {
+      const txtCode = string.charCodeAt(i);
+      // Para mayúsculas
+      if (txtCode >= 65 && txtCode <= 90) {
+        // De ASCII a string de letra cifrada y concatenando cada letra cifrada
+        string2 += String.fromCharCode(((txtCode - 65 + offset) % 26) + 65);
+      // Para minúsculas
+      } else if (txtCode >= 97 && txtCode <= 122) {
+        string2 += String.fromCharCode(((txtCode - 97 + offset) % 26) + 97);
+      } else if (txtCode >= 48 && txtCode <= 57) {
+        string2 += String.fromCharCode(((txtCode - 48 + offset) % 10) + 48);
+      } else if(txtCode === 32){
+        string2 += string[i];
+      }
+      else {
+        string2 += string[i];
+      }
+    }
+    return string2;
+  },
+  decode: (offset, string) => {
+    if (offset === null || offset === 0) {
+      throw new TypeError();
+    }
+    let string2 = '';
+    // Recorrer el string
+    for (let i = 0; i < string.length; i += 1) {
+      const txtCode = string.charCodeAt(i);
+      // Para mayúsculas
+      if (txtCode >= 65 && txtCode <= 90) {
+        // De ASCII a string de letra cifrada y concatenando cada letra cifrada
+        string2 += String.fromCharCode(((txtCode - 90 - offset) % 26) + 90);
+      // Para minúsculas
+      } else if (txtCode >= 97 && txtCode <= 122) {
+        string2 += String.fromCharCode(((txtCode - 122 - offset) % 26) + 122);
+      } else if (txtCode >= 48 && txtCode <= 57) {
+        string2 += String.fromCharCode(((txtCode - 57 - offset) % 10) + 57);
+      } else {
+        string2 += string[i];
+      }
+    }
+    return string2;
+  },
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,62 @@
 import cipher from './cipher.js';
-
-console.log(cipher);
+const inputText = document.getElementById('inputText');
+const outputText = document.getElementById('outputText');
+const outputMsg = document.getElementById('outputMsg');
+// Cifrar
+document.getElementById('encode').addEventListener('click', () => {
+  const offset = parseInt(document.getElementById('displacementValue').value, 10) || '';
+  const str = document.getElementById('inputText').value || '';
+  if (!offset || !str) {
+    alert('Favor de llenar los campos de codigo y mensaje');
+  } else {
+    outputText.value = cipher.encode(offset, str);
+  }
+  if (inputText.value !== '') {
+    outputMsg.innerText = 'Mensaje cifrado';
+  }
+});
+// Descifrar
+document.getElementById('decode').addEventListener('click', () => {
+  const offset = parseInt(document.getElementById('displacementValue').value, 10) || '';
+  const str = document.getElementById('inputText').value || '';
+  if (!offset || !str) {
+    alert('Favor de llenar los campos de codigo y mensaje');
+  } else {
+    outputText.value = cipher.encode(offset, str);
+  }
+  // Poniendo el valor que retorna encode() como valor del input
+  outputText.value = cipher.decode(offset, str);
+  // Si el input no está vacío retornar el mensaje de descifrado
+  if (inputText.value !== '') {
+    outputMsg.innerText = 'Mensaje descifrado';
+  }
+});
+// Evento del botón borrar
+const deleteButton = document.getElementById('delete');
+deleteButton.addEventListener('click', () => {
+  inputText.value = '';
+  outputText.value = '';
+  document.getElementById('displacementValue').value = 0;
+  outputMsg.innerText = 'Resultado';
+});
+// Copiar
+document.getElementById('copy').addEventListener('click', () => {
+  // Copy de texto en textarea
+  const aux = document.createElement('input');
+  aux.setAttribute('value', outputText.value);
+  document.body.appendChild(aux);
+  // Se selecciona el contenido del campo y Copiando el texto seleccionado
+  aux.select();
+  document.execCommand('copy');
+  document.body.removeChild(aux);
+  // Aviso copy
+  const notification = document.createElement('div');
+  notification.classList.add('jstext');
+  // Insertando al DOM el mensaje
+  notification.innerHTML = 'Mensaje copiado';
+  document.body.appendChild(notification);
+  // Confirmo durante 1.2segs y bye
+  setTimeout(() => {
+    document.body.removeChild(notification);
+  }, 1200);
+});
